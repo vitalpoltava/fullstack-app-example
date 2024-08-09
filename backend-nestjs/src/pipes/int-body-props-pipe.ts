4e7313b44c5462f1fc5e6c6606c5fcdc41330b08
Vitaliy Omelkin <vitalii.omelkin@teladochealth.com>
2024-08-09 23:57:57 +0300

    update pipe typings

--- backend-nestjs/src/pipes/int-body-props-pipe.ts
+++ backend-nestjs/src/pipes/int-body-props-pipe.ts
@@ -3,17 +3,19 @@ import {PipeTransform, Injectable} from '@nestjs/common';
 @Injectable()
 export class IntBodyPropsPipe implements PipeTransform {
   transform(value: any) {
-    if (value && value.id && typeof value.id === 'string') {
-      value.id = parseInt(value.id, 10);
-    }
-    if (value && value.productId && typeof value.productId === 'string') {
-      value.productId = parseInt(value.productId, 10);
-    }
-    if (value && value.price && typeof value.price === 'string') {
-      value.price = parseInt(value.price, 10);
-    }
-    if (value && value.ids && Array.isArray(value.ids)) {
-      value.ids = value.ids.map(id => parseInt(id, 10));
+    if (value) {
+      if (value.id && typeof value.id === 'string') {
+        value.id = parseInt(value.id, 10);
+      }
+      if (value.productId && typeof value.productId === 'string') {
+        value.productId = parseInt(value.productId, 10);
+      }
+      if (value.price && typeof value.price === 'string') {
+        value.price = parseInt(value.price, 10);
+      }
+      if (value.ids && Array.isArray(value.ids)) {
+        value.ids = value.ids.map((id: string) => parseInt(id, 10));
+      }
     }
     return value;
   }
